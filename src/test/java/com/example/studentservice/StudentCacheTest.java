5f753763333db918bf3f5b4d2ded0b61842861d8
greegav <greegav@gmail.com>
2021-08-23 10:05:45 +0300

    caching in spring

---
+++ src/test/java/com/example/studentservice/StudentCacheTest.java
@@ -0,0 +1,42 @@
+package com.example.studentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
+
+@SpringBootTest(webEnvironment = NONE)
+public class StudentCacheTest {
+
+    @Autowired
+    private StudentService studentService;
+
+    @MockBean
+    private StudentRepository studentRepository;
+
+    @Test
+    void getStudentById_forMultipleRequests_isRetrivedFromCache(){
+        //given
+        Long id=123L;
+        given(studentRepository.findById(id)).willReturn(Optional.of(new Student(id, "Mark")));
+
+
+
+        //when
+        studentService.getStudentById(id);
+        studentService.getStudentById(id);
+        studentService.getStudentById(id);
+
+
+        //then
+        then(studentRepository).should(times(1)).findById(id);
+
+    }
+}
